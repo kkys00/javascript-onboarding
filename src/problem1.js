aa31f7ff09568961a6864b90b86af9886084e68e
yeongseokim <1903020@ewhain.net>
2022-10-28 19:01:14 +0900

    제한사항 1. pobi와 crong 배열 길이가 2가 아니면 -1 리턴 2. 배열에 [왼쪽 페이지, 오른쪽 페이지]가 제대로 들어 있는지 확인

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,15 @@
+function multDigit(num) {
+
+}
+
 function problem1(pobi, crong) {
-  var answer;
+  if (pobi.length != 2) return -1
+  if (crong.length != 2) return -1
+  if (pobi[1] - pobi[0] != 1) return -1
+  if (crong[1] - crong[0] != 1) return -1
+
+  let answer;
+
   return answer;
 }
 
