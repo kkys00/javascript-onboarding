4bcd819c8dfffdc013cfd8a50e7f1a75c9185f58
yeongseokim <1903020@ewhain.net>
2022-10-31 19:08:53 +0900

    problem6 1. 이름을 두 글자씩 나눈 배열 생성 2. 크루들에 대해 두 글자가 연속으로 겹치는 지 확인 3. 겹치는 크루의 이메일 리턴

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,35 @@
+function splitName(name) {
+  let arr = [];
+  if (name.length <= 2) return [name];
+  for (let i = 0; i < name.length - 1; i++) {
+    arr.push(name.substr(i, 2));
+  }
+  return arr
+}
+
 function problem6(forms) {
   var answer;
+  let dupCrew = [];
+  let flag = false;
+
+  for (let i = 0; i < forms.length - 1; i++) {
+    let splitedName = splitName(forms[i][1]);
+    for (let j = i + 1; j < forms.length; j++) {
+      for (let k = 0; k < splitedName.length; k++) {
+        let target = splitedName[k];
+        if (forms[j][1].match(target)) {
+          dupCrew.push(forms[j][0]);
+          flag = true;
+          break;
+        }
+      }
+    }
+    if (flag) dupCrew.push(forms[i][0]);
+    flag = false;
+  }
+  const set = new Set(dupCrew);
+  answer = [...set];
+  answer.sort();
   return answer;
 }
 
